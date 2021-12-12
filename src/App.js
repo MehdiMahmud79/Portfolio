757ac92ffdb39697641abc7a6b59c796d2597a2c
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-12-12 13:49:02 +0000

    add font awesome

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
     <div className="App">
       <h1 className="bg-red-600 text-red-200">Hellow World</h1>
+      <i className="fas fa-user bg-red-400 text-yellow-200 p-2"></i>
+      <i className="far fa-user"></i>
+      <i className="fal fa-user"></i>
     </div>
   );
 }
