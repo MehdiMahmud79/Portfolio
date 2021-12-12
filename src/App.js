6865cc6d6e7dd6c92dac37863791b2092aafcddd
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-12-12 13:11:54 +0000

    cleaning the app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <h1 className="bg-red-600 text-red-200">Hellow World</h1>
+    </div>
+  );
 }
 
 export default App;
