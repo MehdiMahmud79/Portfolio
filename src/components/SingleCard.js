4722dbb27574c0841c50b2404c588eadcecd3bdd
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-12-17 14:05:15 +0000

    added hover card

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -5,7 +5,7 @@ import logo from "../images/logoSm.png";
 const SingleCard = ({ data }) => {
   return (
     <>
-      <div className="max-w-md py-2 px-4 bg-gray-300 shadow-lg rounded-t-3xl my-5  relative ">
+      <div className="max-w-md py-2 px-4 bg-gray-300 shadow-lg rounded-t-3xl my-5  relative hover:scale-y-105 cursor-alias">
         <div className="flex justify-center md:justify-end my-4">
           <img
             className="w-100 h-100 object-cover my-6 rounded-3xl border-2 border-gray-500 shadow-lg "
@@ -28,7 +28,7 @@ const SingleCard = ({ data }) => {
             target="_blank"
             type="button"
             href={data.gitHub_Url}
-            className=" text-decoration-none hover:text-red-600"
+            className=" text-decoration-none hover:text-yellow-600"
           >
             <i className="fab fa-github-alt"></i> Github
           </a>
@@ -42,7 +42,7 @@ const SingleCard = ({ data }) => {
             target="_blank"
             type="button"
             href={data.homepage}
-            className=" text-decoration-none hover:text-red-600 "
+            className=" text-decoration-none hover:text-yellow-600 "
           >
             <i className="fab fa-edge"></i> Live
           </a>
